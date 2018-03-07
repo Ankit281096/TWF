80f50073835514b1c2e7784eadda296cd8ddd26b
Ankit <ankit.son.og@gmail.com>
2018-03-07 20:43:59 +0530

    changes

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-function myfunction(){
-    var ifsc=document.getElementById('ifsc').value;
-    //console.log(ifsc);
-    //var url= ''
-    console.log(ifsc);
-    console.log('hello');
-    // // var url='https://ifsc.razorpay.com/KARB0000001';
-    
+window.onload = function() {
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+        var ifsc=getParameterByName('ifsc_bank');
+        fetch(`https://ifsc.razorpay.com/${ifsc}`, {
+        method: 'get'
+    }).then(function(response) {
+        console.log(response);
+    }).catch(function(err) {
+        console.log(error);
+    }); 
 }
-fetch('https://ifsc.razorpay.com/KARB0000001', {
-	method: 'get'
-}).then(function(response) {
-	console.log(response);
-}).catch(function(err) {
-	console.log(error);
-});
+
+        
