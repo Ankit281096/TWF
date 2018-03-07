7d3750413158190f06114758a2019faf83e3ec3f
Ankit <ankit.son.og@gmail.com>
2018-03-07 19:36:20 +0530

    commit1

--- main.js
+++ main.js
@@ -4,30 +4,13 @@ function myfunction(){
     //var url= ''
     console.log(ifsc);
     console.log('hello');
-    // var url='https://ifsc.razorpay.com/KARB0000001';
-    callAPI();  
+    // // var url='https://ifsc.razorpay.com/KARB0000001';
+    
 }
-function callAPI(){
-    var getJSON = function(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-          var status = xhr.status;
-          if (status === 200) {
-            callback(null, xhr.response);
-          } else {
-            callback(status, xhr.response);
-          }
-        };
-        xhr.send();
-    };
-getJSON('https://ifsc.razorpay.com/KARB0000001',
-function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-      console.log(data);
-  }
+fetch('https://ifsc.razorpay.com/KARB0000001', {
+	method: 'get'
+}).then(function(response) {
+	console.log(response);
+}).catch(function(err) {
+	console.log(error);
 });
-}
