8af825c17f6c66f10eaa41a468a250bf830260d6
Ankit <ankit.son.og@gmail.com>
2018-03-08 10:46:30 +0530

    styling

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+var data;
 window.onload = function() {
         function getParameterByName(name, url) {
             if (!url) url = window.location.href;
@@ -9,13 +11,53 @@ window.onload = function() {
             return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
         var ifsc=getParameterByName('ifsc_bank');
-        fetch(`https://ifsc.razorpay.com/${ifsc}`, {
-        method: 'get'
-    }).then(function(response) {
-        console.log(response);
+    fetch(`https://ifsc.razorpay.com/${ifsc}`, {
+    method: 'get'
+    }).then(resp=>resp.json())
+    .then(function(response) {
+         if (response === 'Not Found')
+         {
+             window.alert('Wrong IFSC Code');
+        }
+        // console.log(response);
+        data=response;
+         console.log(data);
+        // var table=document.getElementById('detail');
+        // row=table.insertRow(-1);
+        // var cell=row.insertCell(-1);
+        // cell.innerHTML=data.ADDRESS;
+        // var table=document.getElementById('detail');
+        // row=table.insertRow(-1);
+        // var cell=row.insertCell(-1);
+        // cell.innerHTML=data.RTGS;
+        // console.log('Address is',response.ADDRESS);
+        // document.getElementById("detail").innerHTML='<tr><td>'+data.ADDRESS +'</td></tr>'
+        document.getElementById("detail").innerHTML=showObject(data);
+        function showObject(obj) {
+            var result = "";
+            for (var p in obj) {
+              if( obj.hasOwnProperty(p) ) {
+                result +='<tr><td><strong>'+ p + '</strong></td>' +'<td>' + obj[p] + '</td></tr>';
+              } 
+            }              
+            return result;
+          }
     }).catch(function(err) {
-        console.log(error);
+        window.alert('Unable to fetch .. NETWORK ERROR');
     }); 
+    
+
 }
 
-        
+
+
+
+
+// $(document).ready(function () {
+//     $.getJSON('data.json', function(data) {
+//         var tblRow = "<tr>" + "<td>" + data.ADDRESS + "</td>" + "</tr>"
+//          $(tblRow).appendTo("#detail tbody");
+ 
+//  });
+//   });
+  
