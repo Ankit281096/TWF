763b0f116b538dba5e1c23cd59adb143d4291b23
Ankit <ankit.son.og@gmail.com>
2018-03-08 13:05:44 +0530

    final changes

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-
 var data;
 window.onload = function() {
-        function getParameterByName(name, url) {
-            if (!url) url = window.location.href;
-            name = name.replace(/[\[\]]/g, "\\$&");
-            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                results = regex.exec(url);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, " "));
-        }
-        var ifsc=getParameterByName('ifsc_bank');
-        if(ifsc === '')
-        {
-            window.alert("Enter the IFSC Code");
-            window.location.replace("./test.html");
-        }
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    var ifsc=getParameterByName('ifsc_bank');
+    if(ifsc === '')
+    {
+        window.alert("Enter the IFSC Code");
+        window.location.replace("./test.html");
+    }
     fetch(`https://ifsc.razorpay.com/${ifsc}`, {
     method: 'get'
     }).then(resp=>resp.json())
     .then(function(response) {
          if (response === 'Not Found')
-         {
+        {
              window.alert("message");
              window.location.replace("./test.html");
         }
         // console.log(response);
         data=response;
-         console.log(data);
+        //console.log(data);
     
         document.getElementById("detail").innerHTML=showObject(data);
         function showObject(obj) {
@@ -43,8 +42,6 @@ window.onload = function() {
         window.alert('Unable to fetch .. NETWORK ERROR');
         window.location.replace("./test.html");
     }); 
-    
-
 }
 
 
